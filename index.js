6402a33974ce67c2020de7ed46b0fefca04cb2b7
Adriano Lamas <lamas.free@gmail.com>
2021-10-01 19:50:34 -0300

    alt

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.get("/", function (req, res) {
-//   res.send("Pokedex");
-// });
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -19,10 +17,30 @@ app.get("/cadastro", (req, res) => {
   res.render("cadastro");
 });
 
+// app.post("/recebecad", (req, res) => {
+//   const { nome, email } = req.body;
+//   const retorno = { nome: nome, email: email };
+//   res.send(retorno);
+//   //   res.render("retorna", { nome: retorno.nome, email: retorno.email });
+// });
+
 app.post("/recebecad", (req, res) => {
-  const { nome, email } = req.body;
-  res.send({ nome: nome, email: email });
-  //   res.send("Funcionando");
+  const { numero, nome } = req.body;
+  const pokedex = [
+    {
+      numero: 1,
+      nome: "Onyx",
+      tipo: "Pedra/Terrestre",
+      imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/095.png",
+      descricao:
+        "À medida que cava no solo, ele absorve muitos objetos duros. É isso que torna seu corpo tão sólido.",
+      altura: 8.8,
+      peso: 210,
+      categoria: "Cobra de Pedra",
+      habilidade: "Cabeçada de Pedra",
+    },
+  ];
+  res.render("retorna", { pokedex: pokedex });
 });
 
 app.listen(port, () =>
